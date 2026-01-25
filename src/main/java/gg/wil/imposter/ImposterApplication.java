2114a8704d560f2ab5776ccd185054f37740dfdd
xWil <xwildev@gmail.com>
2026-01-25 16:17:08 +0000

    Initial commit

---
+++ src/main/java/gg/wil/imposter/ImposterApplication.java
@@ -0,0 +1,13 @@
+package gg.wil.imposter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImposterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImposterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gg/wil/imposter/ImposterApplicationTests.java
@@ -0,0 +1,13 @@
+package gg.wil.imposter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImposterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

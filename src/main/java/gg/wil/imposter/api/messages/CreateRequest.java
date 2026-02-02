081b8722ed9c3c944bcd58558ee0fe08154f79af
xWil <xwildev@gmail.com>
2026-02-02 00:33:55 +0000

    Remove unused classes

--- src/main/java/gg/wil/imposter/api/messages/CreateRequest.java
+++
@@ -1,3 +0,0 @@
-package gg.wil.imposter.api.messages;
-
-public record CreateRequest(String username) { }
--- src/main/java/gg/wil/imposter/api/messages/JoinRequest.java
+++
@@ -1,3 +0,0 @@
-package gg.wil.imposter.api.messages;
-
-public record JoinRequest(String lobbyCode, String username) { }

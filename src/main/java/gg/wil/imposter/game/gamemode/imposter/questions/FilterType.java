a944cc170600408f027e9fd017d2960a7b2fc0c0
xWil <xwildev@gmail.com>
2026-03-16 15:30:25 +0000

    Implemented secure random for filters

--- src/main/java/gg/wil/imposter/game/gamemode/imposter/questions/FilterType.java
+++ src/main/java/gg/wil/imposter/game/gamemode/imposter/questions/FilterType.java
@@ -3,7 +3,9 @@ package gg.wil.imposter.game.gamemode.imposter.questions;
 import gg.wil.imposter.game.Game;
 import gg.wil.imposter.game.Lobby;
 import gg.wil.imposter.game.Player;
+import gg.wil.imposter.util.ImposterUtil;
 
+import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -19,7 +21,7 @@ public enum FilterType {
         @Override
         public FilteredQuestion filter(QuestionPair pair, Lobby lobby, Game game) {
             List<Player> players = new ArrayList<>(lobby.getPlayers());
-            String randomPlayer = players.get(new Random().nextInt(players.size())).getUsername();
+            String randomPlayer = players.get(secureRandom.nextInt(players.size())).getUsername();
             return new FilteredQuestion(pair.question().replace("%random_player%", randomPlayer), pair.imposterQuestion().replace("%random_player%", randomPlayer));
         }
     },
@@ -27,14 +29,17 @@ public enum FilterType {
         @Override
         public FilteredQuestion filter(QuestionPair pair, Lobby lobby, Game game) {
             List<Player> players = new ArrayList<>(lobby.getPlayers());
-            Random random = new Random();
-            Player group = players.get(random.nextInt(players.size()));
+            Player group = players.get(secureRandom.nextInt(players.size()));
             Player imposter;
-            do { imposter = players.get(random.nextInt(players.size()));
+            do {
+                imposter = players.get(secureRandom.nextInt(players.size()));
             } while(imposter.equals(group));
+
             return new FilteredQuestion(pair.question().replace("%random_player%", group.getUsername()), pair.imposterQuestion().replace("%random_player%", imposter.getUsername()));
         }
     };
+
+    private static final SecureRandom secureRandom = ImposterUtil.generateSecureRandom();
 
     public abstract FilteredQuestion filter(QuestionPair pair, Lobby lobby, Game game);
 }

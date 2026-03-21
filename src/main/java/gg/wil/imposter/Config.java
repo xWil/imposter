c21659ea7595d58f50d793813559af6a85b6beab
xWil <xwildev@gmail.com>
2026-03-21 17:29:35 +0000

    Changed config to get properties from the environment instead of setter methods

--- src/main/java/gg/wil/imposter/Config.java
+++ src/main/java/gg/wil/imposter/Config.java
@@ -1,6 +1,6 @@
 package gg.wil.imposter;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,78 +13,29 @@ public class Config {
     public static int GAME_THREAD_TPS;
 
     public static char[] LOBBY_CODE_ALLOWED_CHARS;
-    public static int LOBBY_CODE_LENGTH = 6;
-    public static int LOBBY_CODE_MAX_ATTEMPTS = 100;
+    public static int LOBBY_CODE_LENGTH;
+    public static int LOBBY_CODE_MAX_ATTEMPTS;
     public static long LOBBY_TIMEOUT;
 
     public static int WEBSOCKET_MAX_CONNECTIONS;
-    public static int WEBSOCKET_MESSAGES_PER_SECOND;
     public static int WEBSOCKET_MAX_SIZE;
+    public static int WEBSOCKET_MESSAGES_PER_SECOND;
     public static String WEBSOCKET_URL;
 
-    // API
-    @Value("${app.api.refill-time}")
-    public void setApiRefillTime(int apiRefillTime) {
-        API_REFILL_TIME = apiRefillTime;
-    }
+    public Config(Environment env) {
+        API_REFILL_TIME = env.getProperty("app.api.refill-time", Integer.class, 0);
+        API_TOKEN_COUNT = env.getProperty("app.api.token-count", Integer.class, 0);
 
-    @Value("${app.api.token-count}")
-    public void setApiTokenCount(int apiTokenCount) {
-        API_TOKEN_COUNT = apiTokenCount;
-    }
+        GAME_THREAD_SLEEP_INACCURACY = env.getProperty("app.game.thread.sleep-inaccuracy", Long.class, 0L);
+        GAME_THREAD_TPS = env.getProperty("app.game.thread.tps", Integer.class, 0);
 
+        LOBBY_CODE_ALLOWED_CHARS = env.getProperty("app.lobby.code.allowed-chars", "").toCharArray();
+        LOBBY_CODE_LENGTH = env.getProperty("app.lobby.code.length", Integer.class, 6);
+        LOBBY_CODE_MAX_ATTEMPTS = env.getProperty("app.lobby.code.max-attempts", Integer.class, 100);
+        LOBBY_TIMEOUT = env.getProperty("app.lobby.timeout", Long.class, 0L);
 
-    // GAME
-    @Value("${app.game.thread.sleep-inaccuracy}")
-    public void setGameThreadSleepInaccuracy(long gameThreadSleepInaccuracy) {
-        GAME_THREAD_SLEEP_INACCURACY = gameThreadSleepInaccuracy;
-    }
-
-    @Value("${app.game.thread.tps}")
-    public void setGameThreadTps(int gameThreadTps) {
-        GAME_THREAD_TPS = gameThreadTps;
-    }
-
-    // LOBBY
-    @Value("${app.lobby.code.allowed-chars}")
-    public void setLobbyCodeAllowedChars(String lobbyCodeAllowedChars) {
-        LOBBY_CODE_ALLOWED_CHARS = lobbyCodeAllowedChars.toCharArray();
-    }
-
-    @Value("${app.lobby.code.length}")
-    public void setLobbyCodeLength(int lobbyCodeLength) {
-        LOBBY_CODE_LENGTH = lobbyCodeLength;
-    }
-
-    @Value("${app.lobby.code.max-attempts}")
-    public void setLobbyCodeMaxAttempts(int lobbyCodeMaxAttempts) {
-        LOBBY_CODE_MAX_ATTEMPTS = lobbyCodeMaxAttempts;
-    }
-
-    @Value("${app.lobby.timeout}")
-    public void setLobbyTimeout(long lobbyTimeout) {
-        LOBBY_TIMEOUT = lobbyTimeout;
-    }
-
-
-    // WEBSOCKET
-    @Value("${app.websocket.max-connections}")
-    public void setWebsocketMaxConnections(int websocketMaxSize) {
-        WEBSOCKET_MAX_CONNECTIONS = websocketMaxSize;
-    }
-
-    @Value("${app.websocket.messages-per-second}")
-    public void setWebsocketMessagesPerSecond(int websocketMessagesPerSecond) {
-        WEBSOCKET_MESSAGES_PER_SECOND = websocketMessagesPerSecond;
-    }
-
-    @Value("${app.websocket.max-size}")
-    public void setWebsocketMaxSize(int websocketMaxSize) {
-        WEBSOCKET_MAX_SIZE = websocketMaxSize;
-    }
-
-    @Value("${app.websocket.url}")
-    public void setWebsocketUrl(String websocketUrl) {
-        WEBSOCKET_URL = websocketUrl;
+        WEBSOCKET_MAX_CONNECTIONS = env.getProperty("app.websocket.max-connections", Integer.class, 0);
+        WEBSOCKET_MAX_SIZE = env.getProperty("app.websocket.max-size", Integer.class, 0);
+        WEBSOCKET_MESSAGES_PER_SECOND = env.getProperty("app.websocket.messages-per-second", Integer.class, 0);WEBSOCKET_URL = env.getProperty("app.websocket.url", "");
     }
 }
